a4c7bad667130f1b189a2c974556338e71616f46
Sky Su <februaryskys@gmail.com>
2016-03-13 21:55:35 +0000

    Adds isDisplaying state. Adds open/close functions, refactors toggle to use new functions

--- menu.js
+++ menu.js
@@ -1,14 +1,31 @@
 var Menu = function() {
     this.menuEl = document.querySelector('#menu');
+
     this.iconEl = document.querySelector('#menu-icon');
     this.iconEl.onclick = this.toggle.bind(this);
+
+    this.isDisplaying = window.innerWidth >= 768;
+    this.menuEl.style.marginLeft = (this.isDisplaying ? '0px' : '-300px');
 };
 
 Menu.prototype = {
 
     toggle: function() {
-            var newMargin = (this.menuEl.style.marginLeft === '0px' ? '-300px' : '0px');
-            this.menuEl.style.marginLeft = newMargin;
+        if (this.isDisplaying) {
+            this.isDisplaying = false;
+            this.close();
+        } else {
+            this.isDisplaying = true;
+            this.open();
         }
+    },
+
+    open: function() {
+        this.menuEl.style.marginLeft = '0px';
+    },
+
+    close: function() {
+        this.menuEl.style.marginLeft = '-300px';
+    }
 
 };
