30a659155b1b03d665a8bf58ca8f0ac0d3ae4e00
Sky Su <februaryskys@gmail.com>
2016-03-13 21:45:24 +0000

    Moves toggle into own function in Menu.prototype

--- menu.js
+++ menu.js
@@ -1,8 +1,14 @@
 var Menu = function() {
     this.menuEl = document.querySelector('#menu');
     this.iconEl = document.querySelector('#menu-icon');
-    this.iconEl.onclick = function() {
-        var newMargin = (this.menuEl.style.marginLeft === '0px' ? '-300px' : '0px');
-        this.menuEl.style.marginLeft = newMargin;
-    }.bind(this);
-}
+    this.iconEl.onclick = this.toggle.bind(this);
+};
+
+Menu.prototype = {
+
+    toggle: function() {
+            var newMargin = (this.menuEl.style.marginLeft === '0px' ? '-300px' : '0px');
+            this.menuEl.style.marginLeft = newMargin;
+        }
+
+};
