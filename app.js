29b0ec7b4a0eb8bef8c24a795ce82cb742d15823
Sky Su <februaryskys@gmail.com>
2016-03-13 22:14:06 +0000

    Adds window.onresize listener to open menu when viewport > 768px

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+
+
 window.onload = function() {
     console.log('app started');
 
     var menu = new Menu();
 
-}
+    window.onresize = function() {
+        if (this.innerWidth > 768) {
+            menu.open();
+        }
+    };
+
+};
