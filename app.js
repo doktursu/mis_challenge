fd6571d878c66e48f067d02cebbbe0c09f4a348e
Sky Su <februaryskys@gmail.com>
2016-03-13 22:29:46 +0000

    Adds isDefaultDisplaying state and setters to menu to handle viewport resize closing

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ window.onload = function() {
 
     window.onresize = function() {
         if (this.innerWidth > 768) {
+            menu.setDefaultOpen();
             menu.open();
+        } else if (menu.isDefaultDisplaying) {
+            menu.close();
+            menu.setDefaultClosed();
         }
     };
 
--- menu.js
+++ menu.js
@@ -5,6 +5,8 @@ var Menu = function() {
     this.iconEl.onclick = this.toggle.bind(this);
 
     this.isDisplaying = window.innerWidth >= 768;
+    this.isDefaultDisplaying = window.innerWidth >= 768;
+
     this.menuEl.style.marginLeft = (this.isDisplaying ? '0px' : '-300px');
     this.iconEl.style.backgroundImage = (this.isDisplaying ? 'url("images/1457924590_ic_close_48px.svg")' : 'url("images/1457921783_menu.svg")');
 };
@@ -13,10 +15,8 @@ Menu.prototype = {
 
     toggle: function() {
         if (this.isDisplaying) {
-            this.isDisplaying = false;
             this.close();
         } else {
-            this.isDisplaying = true;
             this.open();
         }
     },
@@ -24,11 +24,21 @@ Menu.prototype = {
     open: function() {
         this.menuEl.style.marginLeft = '0px';
         this.iconEl.style.backgroundImage = 'url("images/1457924590_ic_close_48px.svg")';
+        this.isDisplaying = true;
     },
 
     close: function() {
         this.menuEl.style.marginLeft = '-300px';
         this.iconEl.style.backgroundImage = 'url("images/1457921783_menu.svg")';
+        this.isDisplaying = false;
+    },
+
+    setDefaultOpen: function() {
+        this.isDefaultDisplaying = true;
+    },
+
+    setDefaultClosed: function() {
+        this.isDefaultDisplaying = false;
     }
 
 };
