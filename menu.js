f94d3e25c4f4c641aa46516cc906a39b3b61a0e1
Sky Su <februaryskys@gmail.com>
2016-03-13 23:17:51 +0000

    Refactors menu to absolute positioning

--- menu.js
+++ menu.js
@@ -7,7 +7,7 @@ var Menu = function() {
     this.isDisplaying = window.innerWidth >= 768;
     this.isDefaultDisplaying = window.innerWidth >= 768;
 
-    this.menuEl.style.marginLeft = (this.isDisplaying ? '0px' : '-300px');
+    this.menuEl.style.left = (this.isDisplaying ? '0px' : '-300px');
     this.iconEl.style.backgroundImage = (this.isDisplaying ? 'url("images/1457924590_ic_close_48px.svg")' : 'url("images/1457921783_menu.svg")');
 };
 
@@ -22,13 +22,13 @@ Menu.prototype = {
     },
 
     open: function() {
-        this.menuEl.style.marginLeft = '0px';
+        this.menuEl.style.left = '0px';
         this.iconEl.style.backgroundImage = 'url("images/1457924590_ic_close_48px.svg")';
         this.isDisplaying = true;
     },
 
     close: function() {
-        this.menuEl.style.marginLeft = '-300px';
+        this.menuEl.style.left = '-300px';
         this.iconEl.style.backgroundImage = 'url("images/1457921783_menu.svg")';
         this.isDisplaying = false;
     },
