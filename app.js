bf1d1d484439a518929f743900a91ebf8859f335
Sky Su <februaryskys@gmail.com>
2016-03-14 00:19:13 +0000

    Adds switch statement for options and greetings

--- app.js
+++ app.js
@@ -10,8 +10,25 @@ window.onload = function() {
         var inputEl = document.querySelector('#name-input');
         var name = inputEl.value;
         var message = "Please enter a name first";
+
+        var greeting;
         if (name) {
-            message = "Hello " + name;
+            switch (key) {
+                case 'say-hello':
+                    greeting = "Hello ";
+                    break;
+
+                case 'say-goodbye':
+                    greeting = "Goodbye ";
+                    break;
+
+                case 'say-thanks':
+                    greeting = "Thanks ";
+                    break;
+
+                default:
+            }
+            message = greeting + name;
         }
         popup.display(message);
 
