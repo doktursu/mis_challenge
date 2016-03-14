29269072e8598cc66b691727ebf384dbc25a9e0d
Sky Su <februaryskys@gmail.com>
2016-03-14 00:06:30 +0000

    Styles popup with display and hidden classes

--- popup.js
+++ popup.js
@@ -6,6 +6,8 @@ Popup.prototype = {
 
     display: function(message) {
         this.popupEl.innerText = message;
+        this.popupEl.className = 'popup-display';
+        console.log(this.popupEl.style.className);
     }
 
 };
